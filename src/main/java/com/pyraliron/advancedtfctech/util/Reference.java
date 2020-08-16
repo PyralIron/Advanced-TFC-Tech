da1a15c02f98990334d8b35d53cde748e8151da7
PyralIron <williamleber@gmail.com>
2020-08-16 06:49:23 -0700

    version 0.1.1.2

--- src/main/java/com/pyraliron/advancedtfctech/util/Reference.java
+++ src/main/java/com/pyraliron/advancedtfctech/util/Reference.java
@@ -3,7 +3,7 @@ package com.pyraliron.advancedtfctech.util;
 public class Reference {
 	public static final String MOD_ID = "att";
 	public static final String NAME = "Advanced TFC Tech";
-	public static final String VERSION = "0.1.1.1";
+	public static final String VERSION = "0.1.1.2";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.pyraliron.advancedtfctech.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.pyraliron.advancedtfctech.proxy.CommonProxy";
